46a8f422565b5bea3731a14344b3b3af914fce2c
Bruno Silva Gomes <bruno1991uk@gmail.com>
2026-07-14 11:14:00 +0100

    feat: add initial HTML and JavaScript files for practicing JavaScript syntax and concepts

---
+++ Practical-Activities-JavaScript/Activity-2.4-Practice-the-syntax-of-JavaScript-concepts/script.js
@@ -0,0 +1,26 @@
+// =============================
+// Variables Practice
+// =============================
+
+// var can be used to declare a variable, but it is older JavaScript syntax.
+var city = "London";
+
+// let is used when the value may change later.`
+let age = 35;
+
+// const is used when the value will not change.
+const name = "Bruno"
+
+// differen t data types
+let isStudent = true; // boolean
+let courseName = "Coding Diploma"; // string
+let completedLessons = 5; // number
+
+//This is just printing each variable with a clear label, so the console is easier to read.
+console.log("City:", city);
+console.log("Age:", age);
+console.log("Name:", name);
+console.log("Is Student:", isStudent);
+console.log("Course Name:", courseName);
+console.log("Completed Lessons:", completedLessons);
+
