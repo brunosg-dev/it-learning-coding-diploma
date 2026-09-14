d88e4810b843f8f3f01030d344e9b605ee79f3b6
Bruno Silva Gomes <bruno1991uk@gmail.com>
2026-09-14 21:47:00 +0100

    feat: Add constructor to BankAccount class with parameters for account details

--- Practical-Activities-JavaScript/Activity-2.5-simple-banking-system/bankAccount.js
+++ Practical-Activities-JavaScript/Activity-2.5-simple-banking-system/bankAccount.js
@@ -1,5 +1,11 @@
 class BankAccount {
 
+    constructor(accountNumber, accountHolder, balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
 }
 
 
@@ -7,3 +13,8 @@ class BankAccount {
 
 
 // class names conventionally use PascalCase: BankAccount, UserProfile, ShoppingCart
+
+/*What is a parameter?
+A parameter is a temporary name that represents information a function or method expects to receive.
+Everyday meaning:
+“I need you to give me some information when you use me.” */
