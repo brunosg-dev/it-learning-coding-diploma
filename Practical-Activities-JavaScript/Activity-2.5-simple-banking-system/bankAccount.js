b5194d5f63419bbc1061fd6d2bd1ba7c164ea616
Bruno Silva Gomes <bruno1991uk@gmail.com>
2026-09-15 15:07:37 +0100

    feat: Enhance BankAccount class with deposit, withdraw, and checkBalance methods

--- Practical-Activities-JavaScript/Activity-2.5-simple-banking-system/bankAccount.js
+++ Practical-Activities-JavaScript/Activity-2.5-simple-banking-system/bankAccount.js
@@ -6,10 +6,26 @@ class BankAccount {
         this.balance = balance;
     }
 
+deposit(amount) {
+    this.balance += amount;
+    console.log(`Deposited: £${amount}. New balance: £${this.balance}`);
 }
 
+withdraw(amount) {
+    if (amount > this.balance) {
+        console.log(`Insufficient funds. Current balance: £${this.balance}`);
+    } else {
+        this.balance -= amount;
+        console.log(`Withdrew: £${amount}. New balance: £${this.balance}`);
+    }
+}
 
+checkBalance() {
+    console.log(`Account balance for ${this.accountHolder}: £${this.balance}`);
+}
+}
 
+export default BankAccount;
 
 
 // class names conventionally use PascalCase: BankAccount, UserProfile, ShoppingCart
@@ -17,4 +33,7 @@ class BankAccount {
 /*What is a parameter?
 A parameter is a temporary name that represents information a function or method expects to receive.
 Everyday meaning:
-“I need you to give me some information when you use me.” */
+“I need you to give me some information when you use me.” 
+
+What does default mean? “This is the main thing this file is providing.”
+*/
